832ad244e6c9fae8b40e6fc825fdb4819376c3a1
wobutianl <wobutianl@126.com>
2014-07-25 14:42:49 +0800

    emacs

---
+++ README.md
@@ -0,0 +1,4 @@
+emacs_config
+============
+
+emacs config (jerry)
---
+++ config/jerry-keybinding.el
@@ -0,0 +1,15 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;; key binding ;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(global-set-key [f1] 'other-window)
+;; 跳转到 Emacs 的另一个buffer窗口
+
+(global-set-key [f2] 'undo)
+(global-set-key [f3] 'redo)
+
+;;set the F4 to kill current buffer
+(global-set-key [f4] 'kill-this-buffer) 
+
+(global-set-key [f9] 'speedbar)
+;;设置f9调用speedbar命令
