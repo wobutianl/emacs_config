8d46cb3dba3d1bb88cb6426aa88bb6bb97dd48b1
zhuxiaolin <zhuxiaolin@pset.suntec.net>
2014-12-29 09:04:22 +0800

    delete todo

--- .emacs.d/org-clock-save.el
+++ .emacs.d/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - ZHUXIAOLIN-PC at <2014-12-05 周五 18:04>
+;; org-persist.el - ZHUXIAOLIN-PC at <2014-12-26 周五 22:36>
 (setq stored-clock-history '(("d:/jerryfive/emacs/config/ToDo/newgtd.org" . 18720) ("d:/jerryfive/emacs/config/ToDo/newgtd.org" . 19703) ("d:/jerryfive/emacs/config/ToDo/newgtd.org" . 20320) ("d:/jerryfive/emacs/config/ToDo/newgtd.org" . 19869) ("d:/jerryfive/emacs/config/ToDo/newgtd.org" . 20440)))
